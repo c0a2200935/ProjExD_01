eb91720a400e6a00a819e58984059f987dedadbd
RyugaIshii <c0a2200935@edu.teu.ac.jp>
2023-11-21 16:33:24 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,19 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     koukaton = pg.image.load ("ex01/fig/3.png")#練習２こうかとん画像生成
     koukaton = pg.transform.flip(koukaton,True,False)#練習２こうかとん画像反転
-    koukaton_list =[koukaton,pg.transform.rotate(koukaton,10)]#練習３リスト生成
+    koukaton_list =[koukaton,pg.transform.rotate(koukaton,5),pg.transform.rotate(koukaton,15),pg.transform.rotate(koukaton,10)]#練習３リスト生成
     tmr = 0
-
+ 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = tmr%3200
+        print(x,tmr)
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(pg.transform.flip(bg_img,True,False), [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         #screen.blit(koukaton,[100,100])#練習２こうかとん画像貼り付け
-        screen.blit(koukaton_list[tmr%2],[300,200])
+        screen.blit(koukaton_list[tmr%3],[300,200])
         pg.display.update()#
         tmr += 1        
         clock.tick(10)
